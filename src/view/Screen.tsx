7fe0fb0b8729f0d424937aa81e59bf3fca28d652
Avantgarde95 <95phm@kaist.ac.kr>
2020-07-05 16:26:53 +0900

    Screen background

--- src/view/Screen.tsx
+++ src/view/Screen.tsx
@@ -3,8 +3,11 @@
 import * as React from 'react';
 import {jsx} from '@emotion/core';
 
+const ScreenBackground = require('../image/ScreenBackground');
+
 export const Screen: React.FC = props => (
     <div css={{
+        background: `#000000 url(${ScreenBackground}) no-repeat center`,
         overflowY: 'hidden',
         width: '100%',
         height: 'calc(100% - 4.5rem)'
