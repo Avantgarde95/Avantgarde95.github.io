4fa97782f4e756b7255698e0de7177ebcb0c3a18
Avantgarde95 <95phm@kaist.ac.kr>
2020-07-24 15:46:33 +0900

    Connect the gallery buttons & project area

--- src/app/ProjectApp.tsx
+++ src/app/ProjectApp.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import {jsx} from '@emotion/core';
+import {css, jsx} from '@emotion/core';
 import {createContext, Fragment, ReactNode, useContext, useState} from 'react';
 
 const Background = require('./image/Coffee');
@@ -11,7 +11,7 @@ const LanguageContext = createContext({
     }
 });
 
-const Language = ({children = {} as ReactNode}) => {
+const LanguageProvider = ({children = {} as ReactNode}) => {
     const [language, setLanguage] = useState('English');
 
     return (
@@ -79,97 +79,136 @@ const English = ({children = {} as ReactNode}) => (
     </Fragment>
 );
 
-const Gallery = ({wideScreenQuery = '', images = [{src: '', name: ''}]}) => (
-    <div css={{
-        display: 'inline-block',
-        position: 'relative',
-        width: '100%',
-        height: '17rem',
-        marginRight: 0,
-        [wideScreenQuery]: {
-            float: 'left',
-            width: '22rem',
-            height: '100%',
-            marginRight: '1rem'
-        }
-    }}>
-        <div css={{
-            position: 'absolute',
-            overflow: 'auto',
-            whiteSpace: 'nowrap',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            [wideScreenQuery]: {
-                whiteSpace: 'normal'
+const ProjectContext = createContext({
+    currentProjectIndex: 0,
+    changeProjectIndex: (projectIndex: number) => {
+    }
+});
+
+const ProjectProvider = ({children = {} as ReactNode}) => {
+    const [projectIndex, setProjectIndex] = useState(0);
+
+    return (
+        <ProjectContext.Provider value={{
+            currentProjectIndex: projectIndex,
+            changeProjectIndex: newProjectIndex => {
+                setProjectIndex(newProjectIndex);
             }
         }}>
-            {images.map(({src, name}) => (
-                <button css={{
-                    title: name,
-                    display: 'inline-block',
-                    cursor: 'pointer',
-                    textAlign: 'center',
-                    width: '20rem',
-                    height: '15rem',
-                    marginRight: '1rem',
-                    marginBottom: 0,
-                    fontSize: '2rem',
-                    color: 'rgba(255, 255, 255, 0)',
-                    boxShadow: '0 0 2px #d9890d, 0 0 4px #f9ab0d',
-                    border: 'solid 1px #ffffff',
-                    backgroundImage: `url(${src})`,
-                    backgroundSize: 'contain',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center',
-                    backgroundColor: '#000000',
-                    '&:hover, &:active, &:focus': {
-                        border: 'solid 1px #f9ab0d',
-                        color: '#ffffff',
-                        textShadow: '0 0 2px #d9890d, 0 0 4px #f9ab0d'
-                    },
-                    [wideScreenQuery]: {
-                        display: 'block',
-                        marginRight: 0,
-                        marginBottom: '1rem'
-                    }
-                }}>
-                    {name}
-                </button>
-            ))}
-        </div>
-    </div>
-);
-
-const Project = ({wideScreenQuery = '', children = {} as ReactNode}) => (
-    <div css={{
-        display: 'inline-block',
-        position: 'relative',
-        width: '100%',
-        height: 'calc(100% - 18rem)',
-        minHeight: '8rem',
-        marginBottom: '1rem',
-        [wideScreenQuery]: {
-            float: 'right',
-            width: 'calc(100% - 23rem)',
-            height: '100%'
-        }
-    }}>
-        <div css={{
-            position: 'absolute',
-            overflow: 'auto',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-        }}>
             {children}
-        </div>
-    </div>
-);
+        </ProjectContext.Provider>
+    );
+};
 
-const Title = ({url = '', children = {} as ReactNode}) => (
+const Gallery = ({wideScreenQuery = '', images = [{src: '', name: ''}]}) => {
+    const changeProjectIndex = useContext(ProjectContext).changeProjectIndex;
+
+    const buttonStyle = css(
+        {
+            title: name,
+            display: 'inline-block',
+            cursor: 'pointer',
+            textAlign: 'center',
+            width: '20rem',
+            height: '15rem',
+            marginRight: '1rem',
+            marginBottom: 0,
+            fontSize: '2rem',
+            color: 'rgba(255, 255, 255, 0)',
+            boxShadow: '0 0 2px #d9890d, 0 0 4px #f9ab0d',
+            border: 'solid 1px #ffffff',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundColor: '#000000',
+            '&:hover, &:active, &:focus': {
+                border: 'solid 1px #f9ab0d',
+                color: '#ffffff',
+                textShadow: '0 0 2px #d9890d, 0 0 4px #f9ab0d'
+            },
+            [wideScreenQuery]: {
+                display: 'block',
+                marginRight: 0,
+                marginBottom: '1rem'
+            }
+        }
+    );
+
+    return (
+        <div css={{
+            display: 'inline-block',
+            position: 'relative',
+            width: '100%',
+            height: '17rem',
+            marginRight: 0,
+            [wideScreenQuery]: {
+                float: 'left',
+                width: '22rem',
+                height: '100%',
+                marginRight: '1rem'
+            }
+        }}>
+            <div css={{
+                position: 'absolute',
+                overflow: 'auto',
+                whiteSpace: 'nowrap',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                [wideScreenQuery]: {
+                    whiteSpace: 'normal'
+                }
+            }}>
+                {images.map(({src, name}, index) => (
+                    <button
+                        css={[buttonStyle, {
+                            backgroundImage: `url(${src})`
+                        }]}
+                        onClick={() => {
+                            changeProjectIndex(index);
+                        }}
+                    >
+                        {name}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const Project = ({wideScreenQuery = '', components = [] as ReactNode[]}) => {
+    const currentProjectIndex = useContext(ProjectContext).currentProjectIndex;
+
+    return (
+        <div css={{
+            display: 'inline-block',
+            position: 'relative',
+            width: '100%',
+            height: 'calc(100% - 18rem)',
+            minHeight: '8rem',
+            marginBottom: '1rem',
+            [wideScreenQuery]: {
+                float: 'right',
+                width: 'calc(100% - 23rem)',
+                height: '100%'
+            }
+        }}>
+            <div css={{
+                position: 'absolute',
+                overflow: 'auto',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0
+            }}>
+                {components[currentProjectIndex]}
+            </div>
+        </div>
+    );
+};
+
+const ProjectName = ({url = '', children = {} as ReactNode}) => (
     <a
         css={{
             display: 'block',
@@ -188,13 +227,61 @@ const Title = ({url = '', children = {} as ReactNode}) => (
     </a>
 );
 
-const Description = ({children = {} as ReactNode}) => (
+const ProjectDescription = ({children = {} as ReactNode}) => (
     <div css={{
         width: '100%'
     }}>
         {children}
     </div>
 );
+
+const projects = [
+    {
+        name: 'PaintTalk',
+        url: 'https://github.com/Avantgarde95/PaintTalk',
+        image: require('./image/PaintTalk'),
+        description: (
+            <Fragment>
+                <Korean>
+                    자연어를 이용한 그림 그리기
+                </Korean>
+                <English>
+                    Draw a picture by using natural language
+                </English>
+            </Fragment>
+        )
+    },
+    {
+        name: 'Image2Term',
+        url: 'https://github.com/Avantgarde95/Image2Term',
+        image: require('./image/Image2Term'),
+        description: (
+            <Fragment>
+                <Korean>
+                    터미널 위에 그림 그리기.
+                </Korean>
+                <English>
+                    Draw an image on the terminal.
+                </English>
+            </Fragment>
+        )
+    },
+    {
+        name: 'archipi-web',
+        url: 'https://github.com/Avantgarde95/archipi-web',
+        image: require('./image/ArchiPi4Web'),
+        description: (
+            <Fragment>
+                <Korean>
+                    아르키메데스의 원주율(π) 계산법. π 데이 (2019.03.14) 기념으로 만듬.
+                </Korean>
+                <English>
+                    Archimedes' method for approximating π. Created for celebrating π day! (2019.03.14)
+                </English>
+            </Fragment>
+        )
+    },
+];
 
 export const ProjectApp = () => {
     const wideScreenQuery = '@media screen and (min-width: 769px)';
@@ -210,41 +297,42 @@ export const ProjectApp = () => {
             textShadow: '0 0 2px #d9890d, 0 0 4px #f9ab0d',
             background: `#000000 url(${Background}) no-repeat center`
         }}>
-            <Language>
-                <div css={{
-                    display: 'table-row',
-                    height: 0
-                }}>
-                    <Name>Projects</Name>
+            <LanguageProvider>
+                <ProjectProvider>
                     <div css={{
-                        marginBottom: '1.5rem'
+                        display: 'table-row',
+                        height: 0
                     }}>
-                        <LanguageButton language={'Korean'}>한국어</LanguageButton>
-                        <LanguageButton language={'English'}>English</LanguageButton>
+                        <Name>Projects</Name>
+                        <div css={{
+                            marginBottom: '1.5rem'
+                        }}>
+                            <LanguageButton language={'Korean'}>한국어</LanguageButton>
+                            <LanguageButton language={'English'}>English</LanguageButton>
+                        </div>
                     </div>
-                </div>
-                <div css={{
-                    display: 'table-row',
-                    height: '100%'
-                }}>
-                    <Project wideScreenQuery={wideScreenQuery}>
-                        <Title url={'https://github.com/Avantgarde95/PaintTalk'}>PaintTalk</Title>
-                        <Description>
-                            <Korean>
-                                자연어를 이용한 그림 그리기
-                            </Korean>
-                            <English>
-                                Draw a picture by using natural language
-                            </English>
-                        </Description>
-                    </Project>
-                    <Gallery wideScreenQuery={wideScreenQuery} images={[
-                        {src: require('./image/PaintTalk'), name: 'PaintTalk'},
-                        {src: require('./image/Image2Term'), name: 'Image2Term'},
-                        {src: require('./image/ArchiPi4Web'), name: 'archipi-web'}
-                    ]}/>
-                </div>
-            </Language>
+                    <div css={{
+                        display: 'table-row',
+                        height: '100%'
+                    }}>
+                        <Project
+                            wideScreenQuery={wideScreenQuery}
+                            components={projects.map(project => (
+                                <Fragment>
+                                    <ProjectName url={project.url}>{project.name}</ProjectName>
+                                    <ProjectDescription>{project.description}</ProjectDescription>
+                                </Fragment>
+                            ))}
+                        />
+                        <Gallery
+                            wideScreenQuery={wideScreenQuery}
+                            images={projects.map(project => (
+                                {src: project.image, name: project.name}
+                            ))}
+                        />
+                    </div>
+                </ProjectProvider>
+            </LanguageProvider>
         </div>
     );
 };
