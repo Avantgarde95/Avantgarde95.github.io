c92c5c4c381b068d3e2179e1596ee681b29b3e39
Avantgarde95 <95phm@kaist.ac.kr>
2020-07-25 20:32:20 +0900

    Directly put polyfills instead of using core-js

--- src/Polyfill.ts
+++ src/Polyfill.ts
@@ -1,2 +1,19 @@
-require('core-js/features/string/virtual/starts-with');
-require('core-js/features/string/virtual/ends-with');
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith.
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (search: string, rawPos: number) {
+        const pos = rawPos > 0 ? rawPos | 0 : 0;
+        return this.substring(pos, pos + search.length) === search;
+    };
+}
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith.
+
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function (search: string, this_len: number) {
+        if (this_len === undefined || this_len > this.length) {
+            this_len = this.length;
+        }
+        return this.substring(this_len - search.length, this_len) === search;
+    };
+}
