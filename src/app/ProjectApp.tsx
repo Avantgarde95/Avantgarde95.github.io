3f7ddb0643346a964869d6c498ff308cfa83a3cf
Avantgarde95 <95phm@kaist.ac.kr>
2020-09-16 03:35:06 +0900

    Remove Context from ProjectApp & Refactor ProjectApp

--- src/app/ProjectApp.tsx
+++ src/app/ProjectApp.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
-import {jsx} from '@emotion/core';
-import {createContext, Fragment, ReactNode, useContext, useState} from 'react';
+import {css, jsx} from '@emotion/core';
+import {Fragment, ReactNode, useState} from 'react';
 import {English, Korean} from '../common/Language';
 import {generateTheme} from './Theme';
 import {App} from './App';
@@ -13,26 +13,6 @@ const theme = generateTheme({
     lightColor: '#f9ab0d',
     darkColor: '#d9890d'
 });
-
-const ProjectContext = createContext({} as {
-    currentProjectIndex: number,
-    changeProjectIndex: (projectIndex: number) => any
-});
-
-const ProjectProvider = ({children = {} as ReactNode}) => {
-    const [projectIndex, setProjectIndex] = useState(0);
-
-    return (
-        <ProjectContext.Provider value={{
-            currentProjectIndex: projectIndex,
-            changeProjectIndex: newProjectIndex => {
-                setProjectIndex(newProjectIndex);
-            }
-        }}>
-            {children}
-        </ProjectContext.Provider>
-    );
-};
 
 const Link = ({url = '', children = {} as ReactNode}) => (
     <a
@@ -50,118 +30,6 @@ const Link = ({url = '', children = {} as ReactNode}) => (
         {children}
     </a>
 );
-
-const Gallery = (
-    {
-        buttonWidth = 0,
-        buttonHeight = 0,
-        wideScreenQuery = '',
-        images = [{src: '', name: ''}]
-    }
-) => {
-    const {changeProjectIndex} = useContext(ProjectContext);
-
-    return (
-        <div css={{
-            display: 'inline-block',
-            position: 'relative',
-            width: '100%',
-            height: `${buttonHeight + 2}rem`,
-            // XXX: Dirty fix for preventing the height from being zero in IE 10.
-            minHeight: `${buttonHeight + 2}rem`,
-            marginRight: 0,
-            [wideScreenQuery]: {
-                float: 'left',
-                width: `${buttonWidth + 2}rem`,
-                height: '100%',
-                marginRight: '1rem'
-            }
-        }}>
-            <div css={{
-                position: 'absolute',
-                overflow: 'auto',
-                whiteSpace: 'nowrap',
-                top: 0,
-                right: 0,
-                bottom: 0,
-                left: 0,
-                [wideScreenQuery]: {
-                    whiteSpace: 'normal'
-                }
-            }}>
-                {images.map(({src, name}, index) => (
-                    <button
-                        css={[theme.boxStyle, theme.highlightStyle, {
-                            title: name,
-                            display: 'inline-block',
-                            cursor: 'pointer',
-                            textAlign: 'center',
-                            width: `${buttonWidth}rem`,
-                            height: `${buttonHeight}rem`,
-                            marginRight: '1rem',
-                            marginBottom: 0,
-                            fontSize: '2rem',
-                            color: 'rgba(0, 0, 0, 0)',
-                            backgroundSize: 'contain',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundPosition: 'center',
-                            backgroundImage: `url(${src})`,
-                            '&:hover, &:active, &:focus': theme.textStyle,
-                            [wideScreenQuery]: {
-                                display: 'block',
-                                marginRight: 0,
-                                marginBottom: '1rem'
-                            }
-                        }]}
-                        onClick={() => {
-                            changeProjectIndex(index);
-                        }}
-                    >
-                        {name}
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
-};
-
-const Project = (
-    {
-        buttonWidth = 0,
-        buttonHeight = 0,
-        wideScreenQuery = '',
-        components = [] as ReactNode[]
-    }
-) => {
-    const {currentProjectIndex} = useContext(ProjectContext);
-
-    return (
-        <div css={{
-            display: 'inline-block',
-            position: 'relative',
-            width: '100%',
-            height: `calc(100% - ${buttonHeight + 3}rem)`,
-            minHeight: '7rem',
-            marginBottom: '1rem',
-            [wideScreenQuery]: {
-                float: 'right',
-                width: `calc(100% - ${buttonWidth + 3}rem)`,
-                height: '100%'
-            }
-        }}>
-            <div css={{
-                position: 'absolute',
-                overflow: 'auto',
-                top: 0,
-                right: 0,
-                bottom: 0,
-                left: 0
-            }}>
-                {components[currentProjectIndex]}
-            </div>
-        </div>
-    );
-};
 
 const ProjectName = ({url = '', children = {} as ReactNode}) => (
     <a
@@ -191,9 +59,107 @@ const ProjectDescription = ({children = {} as ReactNode}) => (
     </div>
 );
 
-const buttonWidth = 16;
-const buttonHeight = 12;
-const wideScreenQuery = '@media (min-width: 769px)';
+const ProjectGallery = () => {
+    const [projectIndex, setProjectIndex] = useState(0);
+    const project = projects[projectIndex];
+
+    const buttonWidth = 16;
+    const buttonHeight = 12;
+    const wideScreenQuery = '@media (min-width: 769px)';
+
+    const buttonStyle = css([theme.boxStyle, theme.highlightStyle, {
+        display: 'inline-block',
+        cursor: 'pointer',
+        textAlign: 'center',
+        width: `${buttonWidth}rem`,
+        height: `${buttonHeight}rem`,
+        marginRight: '1rem',
+        marginBottom: 0,
+        fontSize: '2rem',
+        color: 'rgba(0, 0, 0, 0)',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        '&:hover, &:active, &:focus': theme.textStyle,
+        [wideScreenQuery]: {
+            display: 'block',
+            marginRight: 0,
+            marginBottom: '1rem'
+        }
+    }]);
+
+    return (
+        <Fragment>
+            <div css={{
+                display: 'inline-block',
+                position: 'relative',
+                width: '100%',
+                height: `${buttonHeight + 2}rem`,
+                // XXX: Dirty fix for preventing the height from being zero in IE 10.
+                minHeight: `${buttonHeight + 2}rem`,
+                marginRight: 0,
+                [wideScreenQuery]: {
+                    float: 'left',
+                    width: `${buttonWidth + 2}rem`,
+                    height: '100%',
+                    marginRight: '1rem'
+                }
+            }}>
+                <div css={{
+                    position: 'absolute',
+                    overflow: 'auto',
+                    whiteSpace: 'nowrap',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                    [wideScreenQuery]: {
+                        whiteSpace: 'normal'
+                    }
+                }}>
+                    {projects.map(({image, name}, index) => (
+                        <button
+                            css={[buttonStyle, {
+                                title: name,
+                                backgroundImage: `url(${image})`
+                            }]}
+                            onClick={() => {
+                                setProjectIndex(index);
+                            }}
+                        >
+                            {name}
+                        </button>
+                    ))}
+                </div>
+            </div>
+            <div css={{
+                display: 'inline-block',
+                position: 'relative',
+                width: '100%',
+                height: `calc(100% - ${buttonHeight + 3}rem)`,
+                minHeight: '7rem',
+                marginBottom: '1rem',
+                [wideScreenQuery]: {
+                    float: 'right',
+                    width: `calc(100% - ${buttonWidth + 3}rem)`,
+                    height: '100%'
+                }
+            }}>
+                <div css={{
+                    position: 'absolute',
+                    overflow: 'auto',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0
+                }}>
+                    <ProjectName url={project.url}>{project.name}</ProjectName>
+                    <ProjectDescription>{project.description}</ProjectDescription>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
 
 export const ProjectApp = () => (
     <App koreanTitle={'프로젝트'} englishTitle={'Projects'}>
@@ -231,27 +197,7 @@ export const ProjectApp = () => (
                 display: 'table-row',
                 height: '100%'
             }}>
-                <ProjectProvider>
-                    <Project
-                        buttonWidth={buttonWidth}
-                        buttonHeight={buttonHeight}
-                        wideScreenQuery={wideScreenQuery}
-                        components={projects.map(project => (
-                            <Fragment>
-                                <ProjectName url={project.url}>{project.name}</ProjectName>
-                                <ProjectDescription>{project.description}</ProjectDescription>
-                            </Fragment>
-                        ))}
-                    />
-                    <Gallery
-                        buttonWidth={buttonWidth}
-                        buttonHeight={buttonHeight}
-                        wideScreenQuery={wideScreenQuery}
-                        images={projects.map(project => (
-                            {src: project.image, name: project.name}
-                        ))}
-                    />
-                </ProjectProvider>
+                <ProjectGallery/>
             </div>
         </div>
     </App>
