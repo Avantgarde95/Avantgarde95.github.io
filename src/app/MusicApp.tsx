ec86d812280688bf998c3a9cf20459c84c8cbcbe
Avantgarde95 <95phm@kaist.ac.kr>
2020-09-21 16:30:28 +0900

    Update the description & box style of MusicApp

--- src/app/MusicApp.tsx
+++ src/app/MusicApp.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx */
 
 import {jsx} from '@emotion/core';
+import {ReactNode, useContext} from 'react';
 import {App} from './App';
 import {ThemeContext, ThemeProvider} from './Theme';
-import {ReactNode, useContext} from 'react';
 import {English, Korean} from '../common/Language';
 import {musics} from './Musics';
 
@@ -30,16 +30,36 @@ const Link = ({url = '', children = {} as ReactNode}) => {
     );
 }
 
-const Video = ({id = ''}) => (
-    <div css={{
-        marginBottom: '1.5rem'
-    }}>
-        <iframe
-            allowFullScreen={true}
-            src={`https://www.youtube.com/embed/${id}`}
-        />
-    </div>
-);
+const Video = ({id = ''}) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <div css={{
+            width: '100%',
+            maxWidth: '640px',
+            marginBottom: '1.5rem'
+        }}>
+            <div css={{
+                position: 'relative',
+                height: 0,
+                paddingBottom: '56.25%'
+            }}>
+                <iframe
+                    css={[theme.boxStyle, theme.highlightStyle, {
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%'
+                    }]}
+                    frameBorder={0}
+                    allowFullScreen={true}
+                    src={`https://www.youtube.com/embed/${id}`}
+                />
+            </div>
+        </div>
+    );
+}
 
 const Content = () => {
     const theme = useContext(ThemeContext);
@@ -64,10 +84,15 @@ const Content = () => {
             <div css={{
                 marginBottom: '1.5rem'
             }}>
-                <Korean>모든 영상들을 보려면 <Link url={'https://www.youtube.com/user/Scottparkmusic'}>유튜브</Link>를
-                    방문해주세요.</Korean>
-                <English>To see the all videos, visit <Link
-                    url={'https://www.youtube.com/user/Scottparkmusic'}>YouTube</Link>.</English>
+                <Korean>
+                    영상들을 불러오는데에 시간이 걸리니 잠시 기다려주세요.
+                    모든 영상들을 보려면 <Link url={'https://www.youtube.com/user/Scottparkmusic'}>유튜브</Link>를 방문해주세요.
+                </Korean>
+                <English>
+                    It takes some time to load the videos, so please wait a moment.
+                    To see the all videos, visit <Link
+                    url={'https://www.youtube.com/user/Scottparkmusic'}>YouTube</Link>.
+                </English>
             </div>
             {musics.map(id => <Video id={id}/>)}
         </div>
