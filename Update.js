70678d800d27a1b2d0573956eb48d922124555e9
Avantgarde95 <95phm@kaist.ac.kr>
2020-12-30 05:33:28 +0900

    Use async instead of then()

--- Update.js
+++ Update.js
@@ -5,48 +5,49 @@ function getValue(object, key, defaultValue) {
     return object.hasOwnProperty(key) ? object[key] : defaultValue;
 }
 
-const excludedProjects = [
-    'Avantgarde95',
-    'christmas_tree',
-    'gitignore',
-    'kotlinjstest',
-    'JOGLObjTest',
-    'DuktapeTest',
-    'DeutschOracle',
-    'JythonSieve',
-    'MiniSat4Win',
-    'CppIntro',
-    'MyHaskell',
-    'SchoolLaTeX',
-    'slre',
-    'webpack-es6-sass-setup',
-    'Windows'
-];
+async function updateProjects() {
+    const excludedProjects = [
+        'Avantgarde95',
+        'christmas_tree',
+        'gitignore',
+        'kotlinjstest',
+        'JOGLObjTest',
+        'DuktapeTest',
+        'DeutschOracle',
+        'JythonSieve',
+        'MiniSat4Win',
+        'CppIntro',
+        'MyHaskell',
+        'SchoolLaTeX',
+        'slre',
+        'webpack-es6-sass-setup',
+        'Windows'
+    ];
 
-const alternativeProjectNames = {
-    'Avantgarde95.github.io': 'MyPage',
-    'kaist_cs482_2016_team4': 'Blowing Bubble'
-};
+    const alternativeProjectNames = {
+        'Avantgarde95.github.io': 'MyPage',
+        'kaist_cs482_2016_team4': 'Blowing Bubble'
+    };
 
-const alternativeProjectImagePaths = {
-    'Avantgarde95.github.io': 'image/AboutApp.png',
-    'LiKo': 'TestExample.png',
-    'LangtonAnt': 'ScreenshotSimple.png',
-    'ArchiPi': 'Screenshot7.png',
-    'asciirain': 'image/Screenshot.png',
-    'asciisnow': 'image/Screenshot.png',
-    'guitarsayo': 'Screenshot.jpg',
-    'PaintTalk': 'extra/TreeDemo.png'
-};
+    const alternativeProjectImagePaths = {
+        'Avantgarde95.github.io': 'image/AboutApp.png',
+        'LiKo': 'TestExample.png',
+        'LangtonAnt': 'ScreenshotSimple.png',
+        'ArchiPi': 'Screenshot7.png',
+        'asciirain': 'image/Screenshot.png',
+        'asciisnow': 'image/Screenshot.png',
+        'guitarsayo': 'Screenshot.jpg',
+        'PaintTalk': 'extra/TreeDemo.png'
+    };
 
-fetch('https://api.github.com/users/Avantgarde95/repos?per_page=100', {
-    method: 'get',
-    headers: {'Content-Type': 'application/json'}
-}).then(response => {
-    return response.json();
-}).then(repositories => {
-    const projects = repositories
-        .filter(({name}) => !excludedProjects.includes(name))
+    const response = await fetch('https://api.github.com/users/Avantgarde95/repos?per_page=100', {
+        method: 'get',
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    const allProjects = await response.json();
+
+    const projects = allProjects.filter(({name}) => !excludedProjects.includes(name))
         .map(({name, description}) => ({
             name: getValue(alternativeProjectNames, name, name),
             description: description,
@@ -56,4 +57,8 @@ fetch('https://api.github.com/users/Avantgarde95/repos?per_page=100', {
 
     fs.writeFileSync('./src/app/Projects.json', JSON.stringify(projects, null, 4));
     console.log('Updated the list of the projects!');
-});
+}
+
+(async () => {
+    await updateProjects();
+})();
