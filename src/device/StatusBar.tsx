7d50d0ba9838d1ba5d3f0d96c4929194acdbe620
Avantgarde95 <95phm@kaist.ac.kr>
2020-12-30 17:10:35 +0900

    Different device name according to screen width

--- src/device/StatusBar.tsx
+++ src/device/StatusBar.tsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */
 
 import {jsx} from '@emotion/core';
-import {ReactNode} from 'react';
+import {ReactNode, useContext} from 'react';
 import {faWifi} from '@fortawesome/free-solid-svg-icons/faWifi';
 import {faSignal} from '@fortawesome/free-solid-svg-icons/faSignal';
 import {faBatteryFull} from '@fortawesome/free-solid-svg-icons/faBatteryFull';
 import {Icon} from './Icon';
 import {AMPM, Hour, Minute, TimeProvider} from './Time';
 import {English, Korean} from '../common/Language';
+import {MediaContext} from '../common/Media';
 
 const LeftArea = ({children = null as ReactNode}) => (
     <div css={{
@@ -36,27 +37,47 @@ const Block = ({children = null as ReactNode}) => (
     </span>
 );
 
-export const StatusBar = ({showTime = true}) => (
-    <div css={{
-        boxSizing: 'border-box',
-        width: '100%',
-        height: '2rem',
-        paddingLeft: '0.3rem',
-        paddingRight: '0.3rem',
-        lineHeight: '2rem',
-        color: '#ffffff'
-    }}>
-        <LeftArea>
-            <Block>
-                <Korean>마이폰</Korean>
-                <English>MyPhone</English>
-            </Block>
-        </LeftArea>
-        <RightArea>
-            <Block><Icon definition={faWifi}/></Block>
-            <Block><Icon definition={faSignal}/></Block>
-            <Block><Icon definition={faBatteryFull}/></Block>
-            {showTime && <Block><TimeProvider><Hour/>:<Minute/> <AMPM/></TimeProvider></Block>}
-        </RightArea>
-    </div>
-);
+export const StatusBar = ({showTime = true}) => {
+    const {wideScreenQuery} = useContext(MediaContext);
+
+    return (
+        <div css={{
+            boxSizing: 'border-box',
+            width: '100%',
+            height: '2rem',
+            paddingLeft: '0.3rem',
+            paddingRight: '0.3rem',
+            lineHeight: '2rem',
+            color: '#ffffff'
+        }}>
+            <LeftArea>
+                <Block>
+                    <span css={{
+                        display: 'inline',
+                        [wideScreenQuery]: {
+                            display: 'none'
+                        }
+                    }}>
+                        <Korean>마이폰</Korean>
+                        <English>MyPhone</English>
+                    </span>
+                    <span css={{
+                        display: 'none',
+                        [wideScreenQuery]: {
+                            display: 'inline'
+                        }
+                    }}>
+                        <Korean>마이패드</Korean>
+                        <English>MyPad</English>
+                    </span>
+                </Block>
+            </LeftArea>
+            <RightArea>
+                <Block><Icon definition={faWifi}/></Block>
+                <Block><Icon definition={faSignal}/></Block>
+                <Block><Icon definition={faBatteryFull}/></Block>
+                {showTime && <Block><TimeProvider><Hour/>:<Minute/> <AMPM/></TimeProvider></Block>}
+            </RightArea>
+        </div>
+    );
+};
