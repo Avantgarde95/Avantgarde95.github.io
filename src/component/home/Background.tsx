9e4a53c20392924b49c258049b423a510fdb404f
Avantgarde95 <mathematicianscott@gmail.com>
2021-12-25 21:22:11 +0900

    [feat] Create night sky background for /home
    * Background of /home
      * Night sky background using linear-gradient
      * Fill the screen with the background using position: fixed
      * Use z-index to force status bar to be located in front of the background

--- src/component/home/Background.tsx
+++ src/component/home/Background.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Icon } from 'react-avant/lib/Icon';
 import { faStar } from '@fortawesome/free-regular-svg-icons/faStar';
 
-import { getRandomFloat, getRandomInt, range } from 'util/MathUtils';
+import { getRandomFloat, range } from 'util/MathUtils';
 import styles from 'style/home/Background.module.scss';
 
 /**
@@ -11,13 +11,14 @@ import styles from 'style/home/Background.module.scss';
 export const Background = () => {
     return (
         <div className={styles.background}>
-            {range(0, getRandomInt(5, 8)).map(index => (
+            {range(0, 8).map(index => (
                 <div
                     key={index}
                     className={styles.star}
                     style={{
                         top: `${getRandomFloat(10, 90)}%`,
                         left: `${getRandomFloat(10, 90)}%`,
+                        fontSize: `${getRandomFloat(0.5, 1)}rem`,
                         animationDelay: `${getRandomFloat(0, 3)}s`,
                     }}
                 >
