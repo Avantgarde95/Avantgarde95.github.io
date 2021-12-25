d13fa6039a8749e6ddbded0a21d91897b373fb32
Avantgarde95 <mathematicianscott@gmail.com>
2021-12-25 20:07:28 +0900

    [feat] Display different name on the status bar according to the screen width
    * Name on the status bar
      * Wide screen (width >= 769px) -> 'MyPad'
      * Narrow screen -> 'MyPhone'
    * Remove unnecessary file extension from imports
      * ex. `@use 'Common.scss'` -> `@use 'Common'`

--- src/component/device/StatusBar.tsx
+++ src/component/device/StatusBar.tsx
@@ -4,31 +4,51 @@ import { faWifi } from '@fortawesome/free-solid-svg-icons/faWifi';
 import { faSignal } from '@fortawesome/free-solid-svg-icons/faSignal';
 import { faBatteryFull } from '@fortawesome/free-solid-svg-icons/faBatteryFull';
 
+import { Language } from 'store/Language';
+import { useStoreSelector } from 'store/Store';
 import { AMPM, Hour, Minute } from 'component/device/Clock';
 import styles from 'style/device/StatusBar.module.scss';
+import classNames from 'classnames';
+
+const wideNameMap: Record<Language, string> = {
+    Korean: '마이패드',
+    English: 'MyPad',
+};
+
+const narrowNameMap: Record<Language, string> = {
+    Korean: '마이폰',
+    English: 'MyPhone',
+};
 
 /**
  * Component which represents the bar at the top of the phone(tablet)'s screen.
  * ex. Wifi, battery, etc.
  */
-export const StatusBar = () => (
-    <div className={styles.statusBar}>
-        <div className={styles.left}>
-            <div className={styles.block}>Hunmin Park</div>
+export const StatusBar = () => {
+    const currentLanguage = useStoreSelector(state => state.language.currentLanguage);
+
+    return (
+        <div className={styles.statusBar}>
+            <div className={styles.left}>
+                <div className={classNames(styles.block, styles.wideDeviceName)}>{wideNameMap[currentLanguage]}</div>
+                <div className={classNames(styles.block, styles.narrowDeviceName)}>
+                    {narrowNameMap[currentLanguage]}
+                </div>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.block}>
+                    <Icon definition={faWifi} />
+                </div>
+                <div className={styles.block}>
+                    <Icon definition={faSignal} />
+                </div>
+                <div className={styles.block}>
+                    <Icon definition={faBatteryFull} />
+                </div>
+                <div className={styles.block}>
+                    <Hour />:<Minute /> <AMPM />
+                </div>
+            </div>
         </div>
-        <div className={styles.right}>
-            <div className={styles.block}>
-                <Icon definition={faWifi} />
-            </div>
-            <div className={styles.block}>
-                <Icon definition={faSignal} />
-            </div>
-            <div className={styles.block}>
-                <Icon definition={faBatteryFull} />
-            </div>
-            <div className={styles.block}>
-                <Hour />:<Minute /> <AMPM />
-            </div>
-        </div>
-    </div>
-);
+    );
+};
