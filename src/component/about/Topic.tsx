7fd0e3072d5b6f487dc0253323c831c28eb9056c
Avantgarde95 <mathematicianscott@gmail.com>
2022-01-15 14:02:44 +0900

    [bugfix] Replace the array to Fragment
    * Previous
      * answerMap[key] = Array of the elements
      * Problem: Requires React key
    * New
      * Just replace it to the fragment

--- src/component/about/Topic.tsx
+++ src/component/about/Topic.tsx
@@ -36,35 +36,39 @@ export const questionMap: Record<Topic, ReactNode> = {
 };
 
 /**
- * Map of {topic: [Message elements]}.
+ * Map of {topic: Messges element}.
  */
-export const answerMap: Record<Topic, Array<ReactNode>> = {
-    Job: [
-        <TextMessage>
-            <Korean>개발자로 일하고 있습니다.</Korean>
-            <English>I&apos;m working as a developer.</English>
-        </TextMessage>,
-        <TextMessage>
-            <Korean>컴퓨터 그래픽스와 웹에 관심이 있습니다.</Korean>
-            <English>I&apos;m interested in computer graphics and web.</English>
-        </TextMessage>,
-        <ImageMessage title={'컴퓨터 그래픽스 Computer graphics'} src={'/image/RayTracing.png'} />,
-        <ImageMessage title={'웹 Web'} src={'/image/Code.png'} />,
-    ],
-    Hobbies: [
-        <TextMessage>
-            <Korean>취미로 피아노와 기타를 연주합니다.</Korean>
-            <English>I play piano and guitar as a hobby.</English>
-        </TextMessage>,
-        <ImageMessage title={'기타 Guitar'} src={'/image/Guitar.jpg'} />,
-        <ImageMessage title={'공연 Concert'} src={'/image/Concert.jpg'} />,
-    ],
-    Contacts: [
+export const answerMap: Record<Topic, ReactNode> = {
+    Job: (
+        <>
+            <TextMessage>
+                <Korean>개발자로 일하고 있습니다.</Korean>
+                <English>I&apos;m working as a developer.</English>
+            </TextMessage>
+            <TextMessage>
+                <Korean>컴퓨터 그래픽스와 웹에 관심이 있습니다.</Korean>
+                <English>I&apos;m interested in computer graphics and web.</English>
+            </TextMessage>
+            <ImageMessage title={'컴퓨터 그래픽스 Computer graphics'} src={'/image/RayTracing.png'} />
+            <ImageMessage title={'웹 Web'} src={'/image/Code.png'} />
+        </>
+    ),
+    Hobbies: (
+        <>
+            <TextMessage>
+                <Korean>취미로 피아노와 기타를 연주합니다.</Korean>
+                <English>I play piano and guitar as a hobby.</English>
+            </TextMessage>
+            <ImageMessage title={'기타 Guitar'} src={'/image/Guitar.jpg'} />
+            <ImageMessage title={'공연 Concert'} src={'/image/Concert.jpg'} />
+        </>
+    ),
+    Contacts: (
         <TextMessage>
             <Korean>이메일</Korean>
             <English>Email</English>
             :&nbsp;
             <WrappedLink href={'mailto:mathematicianscott@gmail.com'}>mathematicianscott@gmail.com</WrappedLink>
-        </TextMessage>,
-    ],
+        </TextMessage>
+    ),
 };
