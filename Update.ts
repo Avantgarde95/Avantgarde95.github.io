c831eab3c3564c5985b09990f5e1a79fb8689224
Avantgarde95 <mathematicianscott@gmail.com>
2022-01-15 16:46:44 +0900

    [refactor] Migrate update script from js to ts
    * Temporary

--- Update.ts
+++ Update.ts
@@ -17,11 +17,15 @@ import Secret from './Secret.json';
  * }
  */
 
-function getValue(object, key, defaultValue) {
+function getValue<Target, Value>(object: Target, key: PropertyKey, defaultValue: Value) {
     return object.hasOwnProperty(key) ? object[key] : defaultValue;
 }
 
-async function request(url, parameterMap, headerMap) {
+async function request<Value>(
+    url: string,
+    parameterMap: Record<string, string | number | boolean>,
+    headerMap: Record<string, string>
+) {
     const query = Object.getOwnPropertyNames(parameterMap)
         .map(key => key + '=' + encodeURIComponent(parameterMap[key]))
         .join('&');
@@ -31,13 +35,58 @@ async function request(url, parameterMap, headerMap) {
         headers: Object.assign({ 'Content-Type': 'application/json' }, headerMap)
     });
 
-    return response.json();
+    return response.json() as Promise<Value>;
 }
 
-function toBase64(value) {
+function toBase64(value: string) {
     return Buffer.from(value).toString('base64');
 }
 
+/**
+ * Information of each GitHub repository.
+ */
+interface Repository {
+    name: string;
+    description: string;
+}
+
+/**
+ * Map about how much each programming is used.
+ */
+type LanguageMap = Record<string, number>;
+
+/**
+ * (Final) information of each project constructed from the inputs.
+ */
+interface Project {
+    name: string;
+    description: string;
+    repositoryURL: string;
+    imageURL: string;
+    languageMap: LanguageMap;
+}
+
+/**
+ * Information of each YouTube video.
+ */
+interface Video {
+    snippet: {
+        resourceId: {
+            videoId: string;
+        }
+    }
+}
+
+/**
+ * Information of each YouTube playlist.
+ */
+interface PlayList {
+    items: Array<Video>;
+}
+
+/**
+ * Download the projects' information from GitHub.
+ */
 async function updateProjects() {
     const excludedProjects = [
         'Avantgarde95',
@@ -58,7 +107,8 @@ async function updateProjects() {
         'SetTerm',
         'ExtractMailFiles',
         'jsoncpp',
-        'cpp-practice'
+        'cpp-practice',
+        'gh4a'
     ];
 
     const alternativeProjectNames = {
@@ -77,28 +127,28 @@ async function updateProjects() {
         'PaintTalk': 'extra/TreeDemo.png'
     };
 
-    const allProjects = await request(`https://api.github.com/users/Avantgarde95/repos`, {
+    const allProjects = await request<Array<Repository>>(`https://api.github.com/users/Avantgarde95/repos`, {
         per_page: 100
     }, {
         'Authorization': `Basic ${toBase64(Secret.GitHub.id + ':' + Secret.GitHub.key)}`
     });
 
-    const projects = await Promise.all(
+    const projects: Array<Project> = await Promise.all(
         allProjects
             .filter(({ name }) => !excludedProjects.includes(name))
             .map(async ({ name, description }) => {
                 const imagePath = getValue(alternativeProjectImagePaths, name, 'Screenshot.png');
 
-                const languageMap = await request(`https://api.github.com/repos/Avantgarde95/${name}/languages`, {}, {
+                const languageMap = await request<LanguageMap>(`https://api.github.com/repos/Avantgarde95/${name}/languages`, {}, {
                     'Authorization': `Basic ${toBase64(Secret.GitHub.id + ':' + Secret.GitHub.key)}`
                 });
 
                 return {
                     name: getValue(alternativeProjectNames, name, name),
-                    description: description,
+                    description,
                     repositoryURL: `https://github.com/Avantgarde95/${name}`,
                     imageURL: `https://raw.githubusercontent.com/Avantgarde95/${name}/master/${imagePath}`,
-                    languageMap: languageMap
+                    languageMap
                 };
             })
     );
@@ -107,19 +157,25 @@ async function updateProjects() {
     console.log(`Got ${projects.length} projects from GitHub!`);
 }
 
+/**
+ * Get the videos of the playlist.
+ */
 async function getVideos(playlistId) {
-    const result = await request('https://www.googleapis.com/youtube/v3/playlistItems', {
+    const result = await request<PlayList>('https://www.googleapis.com/youtube/v3/playlistItems', {
         playlistId: playlistId,
         part: 'snippet',
         maxResults: 50,
         key: Secret.YouTube.key
-    });
+    }, {});
 
     console.log(`Got ${result.items.length} videos from the YouTube playlist ${playlistId}!`);
 
     return result.items.map(item => item.snippet.resourceId.videoId);
 }
 
+/**
+ * Download the videos' information from YouTube.
+ */
 async function updateMusics() {
     const musics = {
         myGuitarPlayings: await getVideos('PLosnnDHctFV1rm0K7jrSsQeCDBj04Eb70'),
