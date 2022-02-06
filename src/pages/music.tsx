7c82137e43ffc47f33afa6d775c3ec14736167d9
Avantgarde95 <mathematicianscott@gmail.com>
2022-02-06 17:45:09 +0900

    [feat] Create 'music' page
    * Create a blank page for /music...

---
+++ src/pages/music.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from 'style/music/Page.module.scss';
+
+/**
+ * 'Music(s)' page.
+ */
+const Page = () => <div className={styles.page}>Musics!</div>;
+
+export default Page;
