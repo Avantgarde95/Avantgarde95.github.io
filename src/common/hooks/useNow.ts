be6060861ccb64f28539169a8b03d675d575b4e1
Avantgarde95 <mathematicianscott@gmail.com>
2022-09-20 16:06:45 +0900

    [bugfix] Make useNow() SSR-compatible
    * Hydration bug in useNow()
      * Results of `new Date()` at server and client are different
      * So we only compute date at client

--- src/common/hooks/useNow.ts
+++ src/common/hooks/useNow.ts
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
 
-export default function useNow() {
-  const [now, setNow] = useState(new Date());
+interface Time {
+  year: number;
+  month: number;
+  monthDay: number;
+  hour: number;
+  minute: number;
+  second: number;
+}
+
+export default function useNow(): Time {
+  // If we use new Date() at here, server HTML and client HTML won't match.
+  // So we don't compute date before useEffect().
+  const [now, setNow] = useState<Date | null>(null);
 
   useEffect(() => {
+    setNow(new Date());
+
     const interval = setInterval(() => {
       setNow(new Date());
     }, 1000 * 10);
@@ -13,12 +26,21 @@ export default function useNow() {
     };
   }, []);
 
-  return {
-    year: now.getFullYear(),
-    month: now.getMonth() + 1,
-    monthDay: now.getDate(),
-    hour: now.getHours(),
-    minute: now.getMinutes(),
-    second: now.getSeconds(),
-  };
+  return now === null
+    ? {
+        year: 0,
+        month: 0,
+        monthDay: 0,
+        hour: 0,
+        minute: 0,
+        second: 0,
+      }
+    : {
+        year: now.getFullYear(),
+        month: now.getMonth() + 1,
+        monthDay: now.getDate(),
+        hour: now.getHours(),
+        minute: now.getMinutes(),
+        second: now.getSeconds(),
+      };
 }
