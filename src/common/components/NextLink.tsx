066b9d8ee7a88639240202535cdaf8e2486f6ed1
Avantgarde95 <mathematicianscott@gmail.com>
2022-11-08 19:59:46 +0900

    [refactor] Update the packages
    * Next.js 12 -> 13
      * We have to modify NextLink
      * We don't migrate `pages/` to `app/`, since `app/` doesn't support `next export` yet.
    * Update other packages and yarn

--- src/common/components/NextLink.tsx
+++ src/common/components/NextLink.tsx
@@ -1,23 +1,28 @@
 import { ComponentProps, forwardRef } from "react";
 import Link from "next/link";
 
+type NextLinkProps = Omit<ComponentProps<typeof Link>, "href"> & { href?: string };
+
 /**
  * Wrap Next.js Link to enable styling it with Emotion.
  */
-const NextLink = forwardRef<HTMLAnchorElement, ComponentProps<"a">>(({ href = "/", children, ...others }, ref) => (
-  <Link href={href}>
-    <a
-      ref={ref}
-      // Open external links at the new tabs.
-      {...(!href?.startsWith("/") && { target: "_blank", rel: "noreferrer noopener" })}
-      {...others}
-    >
-      {/* We separate `children` from `others` to avoid anchor-has-content lint error. */}
-      {children}
-    </a>
+const NextLink = forwardRef<HTMLAnchorElement, NextLinkProps>(({ href = "/", children, ...others }, ref) => (
+  <Link
+    ref={ref}
+    href={href}
+    // Open external links at the new tabs.
+    {...(!isInternalLink(href) && { target: "_blank", rel: "noreferrer noopener" })}
+    {...others}
+  >
+    {/* We separate `children` from `others` to avoid anchor-has-content lint error. */}
+    {children}
   </Link>
 ));
 
 NextLink.displayName = "NextLink";
+
+function isInternalLink(href: string) {
+  return href.startsWith("/");
+}
 
 export default NextLink;
