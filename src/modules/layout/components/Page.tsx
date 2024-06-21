a59e2ad0cbab906c05c758ae27a9968b8a1ebfe4
Avantgarde95 <mathematicianscott@gmail.com>
2024-06-21 16:59:48 +0900

    [chore] Remove StatusBar
    * Phone concept doesn't fit with content

--- src/modules/layout/components/Page.tsx
+++ src/modules/layout/components/Page.tsx
@@ -3,7 +3,6 @@ import { useRecoilValue } from "recoil";
 import styled from "@emotion/styled";
 
 import { Language, languageState } from "common/states/Language";
-import StatusBar from "modules/layout/components/StatusBar";
 import Header from "modules/layout/components/Header";
 
 interface PageProps {
@@ -17,7 +16,6 @@ const Page = ({ title, children }: PageProps) => {
   return (
     <Container>
       <Inner>
-        <StatusBar />
         <Header title={title[language]} />
         <Content>{children}</Content>
       </Inner>
@@ -29,9 +27,11 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  box-sizing: border-box;
 
   width: 100%;
   height: 100%;
+  padding-top: 8px;
 
   color: ${({ theme }) => theme.color.black};
   background-color: ${({ theme }) => theme.color.white};
