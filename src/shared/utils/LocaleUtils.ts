d98406a0ddd801fedff12f26d701ddc3a12e44d7
Avantgarde95 <mathematicianscott@gmail.com>
2025-06-08 19:20:35 +0900

    [feat] Migrate home page
    * Migrate PageMenu, HistoryView, DecoratedLink
    * Migrate contents
    * Icons: react-icons -> lucide (normal) + simple-icons (brand)

--- src/shared/utils/LocaleUtils.ts
+++ src/shared/utils/LocaleUtils.ts
@@ -1,7 +1,11 @@
-import { localeNames, type Locale } from "@/shared/constants/I18n";
+import { defaultLocale, localeNames, type Locale } from "@/shared/constants/I18n";
 
 // ex. ^/(ko|en)
 const localeRegex = new RegExp(`^/(${localeNames.join("|")})`);
+
+export function getLocale(params: Record<string, string | undefined>) {
+  return (params.locale ?? defaultLocale) as Locale;
+}
 
 export function setLocale(locale: Locale) {
   // ex. /ko -> /en
